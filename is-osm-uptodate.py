55e18a6fe94f795f94ee7bda81b96b3ff09e345f
Francesco Frassinelli <fraph24@gmail.com>
2017-08-18 00:37:12 +0200

    API: Overpass replaced with OSM

--- is-osm-uptodate.py
+++ is-osm-uptodate.py
@@ -2,12 +2,14 @@
 
 import hug
 
+import gzip
 import json
-import tempfile
 import shlex
 import shutil
 import sqlite3
 import subprocess
+from tempfile import NamedTemporaryFile as NamedTemp
+import urllib.request
 
 QUERY = """
 SELECT json_object(
@@ -28,18 +30,13 @@ SELECT json_object(
               json_group_object(k, v) AS attributes
          FROM osm_node_tags
         GROUP BY node_id)
- ON node_id = id;
+ ON node_id = id
+WHERE MbrWithin(Geometry, BuildMbr(?, ?, ?, ?));
 """
 
-COMMAND_TEMPLATE = """
-spatialite_osm_overpass
--mode RAW
--minx {minx}
--miny {miny}
--maxx {maxx}
--maxy {maxy}
--d {path}
-"""
+REQUEST_TEMPLATE = ("https://api.openstreetmap.org"
+    "/api/0.6/map?bbox={minx},{miny},{maxx},{maxy}")
+COMMAND_TEMPLATE = "spatialite_osm_raw -o {} -d {}"
 
 executable = COMMAND_TEMPLATE.split()[0]
 if not shutil.which(executable):
@@ -52,15 +49,20 @@ def getNodes(minx: hug.types.float_number,
         miny: hug.types.float_number,
         maxy: hug.types.float_number,
         output=hug.output_format.json):
-    with tempfile.NamedTemporaryFile() as db:
-        path = db.name
-        command = shlex.split(COMMAND_TEMPLATE.format(**locals()))
+    with NamedTemp() as db, NamedTemp(suffix='.osm') as osm:
+        request = urllib.request.Request(
+            REQUEST_TEMPLATE.format(**locals()),
+            headers={"Accept-Encoding":"gzip"})
+        with urllib.request.urlopen(request) as response:
+            gzipFile = gzip.GzipFile(fileobj=response)
+            shutil.copyfileobj(gzipFile, osm)
+        command = shlex.split(COMMAND_TEMPLATE.format(osm.name, db.name))
         subprocess.run(command, stdout=subprocess.DEVNULL, check=True)
         with sqlite3.connect(db.name) as conn:
             conn.enable_load_extension(True)
             conn.load_extension('mod_spatialite')
             cursor = conn.cursor()
-            cursor.execute(QUERY)
+            cursor.execute(QUERY, (minx, miny, maxx, maxy))
             result = cursor.fetchone()[0]
     return json.loads(result)
 
