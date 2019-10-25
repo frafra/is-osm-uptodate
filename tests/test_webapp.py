ffd53d533d0ae2f5d28dd4f466c7b2aad4949f19
Francesco Frassinelli <fraph24@gmail.com>
2019-10-25 23:13:08 +0200

    Use multiple test for each button

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -1,43 +1,39 @@
 from seleniumbase import BaseCase
 from seleniumbase.config import settings
+from parameterized import parameterized
 
 import shlex
 import subprocess
 
 uwsgi_command = shlex.split('uwsgi --ini uwsgi.ini')
+buttons = 'creation lastedit revisions contributors frequency'.split()
+
+def setup_module():
+    global uwsgi
+    uwsgi = subprocess.Popen(uwsgi_command, cwd='..')
+
+def teardown_module():
+    global uwsgi
+    uwsgi.kill()
 
 class TestWebapp(BaseCase):
-    def setUp(self):
-        super(TestWebapp, self).setUp()
-        self.uwsgi = subprocess.Popen(uwsgi_command, cwd='..')
+    def test_open_home(self):
+        self.set_window_size('640', '480')
+        self.open("http://localhost:8000")
+        self.assert_element("#map path.leaflet-interactive",
+            timeout=settings.EXTREME_TIMEOUT)
 
-    def tearDown(self):
-        self.uwsgi.terminate()
-        super(TestWebapp, self).tearDown()
-
-    def open_home(func):
-        def wrapper(self, *args, **kwargs):
-            self.set_window_size('640', '480')
-            self.open("http://localhost:8000")
-            self.assert_element("#map path.leaflet-interactive",
-                timeout=settings.EXTREME_TIMEOUT)
-        return wrapper
-
-    @open_home
-    def test_results(self):
-        pass
-
-    @open_home
     def test_js_errors(self):
+        self.test_open_home()
         self.assert_no_js_errors()
 
-    @open_home
     def test_404_errors(self):
+        self.test_open_home()
         self.assert_no_404_errors()
 
-    @open_home
-    def test_tabs(self):
-        for button in self.find_elements('#mode input'):
-            self.click(button)
-            self.assert_no_js_errors()
-            self.assert_no_404_errors()
+    @parameterized.expand(buttons)
+    def test_tabs(self, button):
+        self.test_open_home()
+        self.execute_script('document.getElementById("'+button+'").click()')
+        self.assert_no_js_errors()
+        self.assert_no_404_errors()
