9aec5a17565c423dfcfc5b63bc9733ae8e187e28
Francesco Frassinelli <fraph24@gmail.com>
2022-05-10 00:16:21 +0200

    Show computed PNG tiles

--- web/src/constants.js
+++ web/src/constants.js
@@ -3,6 +3,7 @@ export const defaultLocation = [maxZoom, 45.46423, 9.19073];
 
 export const customAttribution = `<a href="https://github.com/frafra/is-osm-uptodate">${document.title}</a> | <a href="https://api.ohsome.org/">ohsome API</a> | &copy; <a href="https://ohsome.org/copyrights">OpenStreetMap contributors</a>`;
 export const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+export const dataTileURL = 'tiles/{z}/{x}/{y}.png';
 
 export const states = {
   LOADING: 'loading',
