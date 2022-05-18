898d6411433a1525b1be548c6f832aac920c942a
Francesco Frassinelli <fraph24@gmail.com>
2022-05-18 10:39:39 +0200

    Fetch data and handle aggregation automatically

--- web/src/constants.js
+++ web/src/constants.js
@@ -1,6 +1,6 @@
 export const maxZoom = 19;
 export const minZoom = 13;
-export const defaultLocation = [maxZoom, 45.46423, 9.19073];
+export const defaultLocation = [17, 45.46423, 9.19073];
 
 export const customAttribution = `<a href="https://github.com/frafra/is-osm-uptodate">${document.title}</a> | <a href="https://api.ohsome.org/">ohsome API</a> | &copy; <a href="https://ohsome.org/copyrights">OpenStreetMap contributors</a>`;
 export const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
