cd676a956fe9bc58c4fdea95070ea839642cd9fa
Francesco Frassinelli <fraph24@gmail.com>
2022-05-28 22:38:38 +0200

    Re-enable Sentry on backend

--- server/app.py
+++ server/app.py
@@ -1,10 +1,21 @@
+import sentry_sdk
 from aiohttp import web
+from sentry_sdk.integrations.aiohttp import AioHttpIntegration
 
+from server import SENTRY_DSN, __version__
 from server.feature import getFeature
 from server.geojson import getData
 from server.static import entry
 from server.statistics import getStats
 from server.tile import tile
+
+if SENTRY_DSN:
+    sentry_sdk.init(
+        SENTRY_DSN,
+        integrations=[AioHttpIntegration()],
+        traces_sample_rate=1.0,
+        release=__version__,
+    )
 
 
 async def webapp():
