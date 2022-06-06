a0a66ba251a8c9f4db2834a43d43f1a189c5ebc4
Francesco Frassinelli <fraph24@gmail.com>
2022-06-06 18:44:29 +0200

    Improve settings/filters UI

--- web/src/components/settings.js
+++ web/src/components/settings.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 
-const classNames = require('classnames');
-
 function Settings({ setFilter, setCustomBoundaries }) {
   const [text, setText] = useState('');
   const [fileUploaded, setFileUploaded] = useState(false);
@@ -9,6 +7,12 @@ function Settings({ setFilter, setCustomBoundaries }) {
   function handleSubmit(e) {
     e.preventDefault();
     setFilter(text);
+  }
+
+  function removeText() {
+    const filterForm = document.getElementById('filterForm');
+    filterForm.reset();
+    setText('');
   }
 
   function uploadFile(e) {
@@ -33,38 +37,62 @@ function Settings({ setFilter, setCustomBoundaries }) {
 
   return (
     <>
-      <form className="input-group" onSubmit={handleSubmit}>
-        <input
-          className="form-control"
-          type="text"
-          placeholder="example: amenity=*"
-          onChange={(event) => setText(event.target.value.trim())}
-        />
-        <input className="btn btn-primary" type="submit" value="Set" />
+      <form id="filterForm" className="form-inline" onSubmit={handleSubmit}>
+        <label htmlFor="filter">
+          Filter nodes using{' '}
+          <a
+            href="https://docs.ohsome.org/ohsome-api/v1/filter.html#selectors"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Ohsome selectors
+          </a>
+        </label>
+        <div className="input-group">
+          <input
+            id="filter"
+            className="form-control"
+            type="text"
+            placeholder="example: amenity=*"
+            onChange={(event) => setText(event.target.value.trim())}
+          />
+          <input className="btn btn-primary" type="submit" value="Set" />
+          {text && (
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={removeText}
+            >
+              <i className="fa fa-trash" />
+            </button>
+          )}
+        </div>
       </form>
       <br />
       <form
         id="boundariesForm"
-        className="input-group"
+        className="form-inline"
         onSubmit={(e) => e.preventDefault()}
       >
-        <label htmlFor="boundaries">Upload custom boundaries (GeoJSON)</label>
-        <input
-          className="form-control"
-          type="file"
-          id="boundaries"
-          name="boundaries"
-          onChange={uploadFile}
-        />
-        <button
-          type="button"
-          className={classNames('btn', 'btn-secondary', {
-            disabled: !fileUploaded,
-          })}
-          onClick={removeFile}
-        >
-          <i className="fa fa-trash" />
-        </button>
+        <label htmlFor="boundaries">Set custom GeoJSON boundaries</label>
+        <div className="input-group">
+          <input
+            id="boundaries"
+            className="form-control"
+            type="file"
+            name="boundaries"
+            onChange={uploadFile}
+          />
+          {fileUploaded && (
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={removeFile}
+            >
+              <i className="fa fa-trash" />
+            </button>
+          )}
+        </div>
       </form>
     </>
   );
